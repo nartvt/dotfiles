ef6e108d7c939c847b8a976b5f16b53e4d59ebaf
nartvt <nartvt90@gmail.com>
2025-07-23 21:28:47 +0700

    enhance project structure

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -19,27 +19,6 @@ lspconfig["dartls"].setup {
     capabilities = capabilities
 }
 
--- Removed the problematic require("java").setup() call
-
-require("supermaven-nvim").setup({
-    keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-    },
-    ignore_filetypes = { cpp = true }, -- or { "cpp", }
-    color = {
-        suggestion_color = "#ffffff",
-        cterm = 244,
-    },
-    log_level = "info",                -- set to "off" to disable logging completely
-    disable_inline_completion = false, -- disables inline completion for use with cmp
-    disable_keymaps = false,           -- disables built in keymaps for more manual control
-    condition = function()
-        return false
-    end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-})
-
 -- Configure JDTLS manually instead of using nvim-java
 local lombok_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls/lombok.jar"
 
